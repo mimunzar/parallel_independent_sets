10f304b15a39edc619357abecc0c6130ccf60067
Jakub Sochor <jakub@sochor.me>
2013-11-09 18:47:36 +0100

    Added script for generation of whole datasets.

---
+++ graphs/generate_graphs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+VERTICES_MIN=100
+VERTICES_MAX=300
+VERTICES_STEP=5
+
+EDGES_MIN=0.1
+EDGES_MAX=0.7
+EDGES_STEP=0.2
+
+GENERATOR="./gen_gml.py"
+
+echo "Cleaning all *.gml files from current directory"
+for f in `find . -maxdepth 1 -name '*.gml'`
+do
+    rm -i $f
+done
+
+for ratio in `seq $EDGES_MIN $EDGES_STEP $EDGES_MAX`
+do
+    echo "Generating graphs for $ratio"
+    rm -rf $ratio
+    for vertices in `seq $VERTICES_MIN $VERTICES_STEP $VERTICES_MAX`
+    do
+        $GENERATOR -v $vertices -e $ratio
+        ret=$?
+        if [ $ret -ne 0 ]; then
+            echo "Generator returned non zero return code. Exiting..."
+            exit
+        fi
+    done
+    mkdir $ratio
+    mv *.gml $ratio
+done
